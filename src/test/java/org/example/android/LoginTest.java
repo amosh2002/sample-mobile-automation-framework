eae2d70beab826564ace330dc5370b3b25f148d1
armen-akyan <armen.armenakyan@picsart.com>
2023-05-29 01:36:21 +0400

    Sample project running on Android

---
+++ src/test/java/org/example/android/LoginTest.java
@@ -0,0 +1,12 @@
+package org.example.android;
+
+import org.example.base.TestBase;
+import org.testng.annotations.Test;
+
+public class LoginTest extends TestBase {
+
+    @Test
+    public void t1() {
+        int i = 0;
+    }
+}
---
+++ src/test/java/org/example/base/DriverUtils.java
@@ -0,0 +1,8 @@
+package org.example.base;
+
+
+import io.appium.java_client.AppiumDriver;
+
+public class DriverUtils {
+    public static AppiumDriver driver;
+}
